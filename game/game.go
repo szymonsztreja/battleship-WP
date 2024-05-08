529e1d9336011f59013a711934579e41feb0aab3
szymonsztreja <s26048@pjwstk.edu.pl>
2024-05-08 14:05:47 -0400

    Cleaned up the game logic

--- game/game.go
+++ game/game.go
@@ -11,90 +11,95 @@ import (
 type Game struct {
 }
 
-func (g Game) Run() {
+func (Game) Run() {
 	gameClient := &client.GameClient{}
 	gameStatus := &client.GameStatus{}
-	boardStruct := client.BoardStruct{}
-	fireResponse := &client.FireResponse{}
-
 	var err error
 
 	gameClient.InitGame()
+	waitForGame(gameClient)
+	yourShips := getBoardGame(gameClient)
 
+	b := board.New(board.NewConfig())
+	err = b.Import(yourShips)
+	if err != nil {
+		fmt.Println(err)
+	}
 	for {
-		gameStatus, err = gameClient.Status()
+		status := getGameStatus(gameClient)
 
-		fmt.Printf("Game status response: %+v\n", gameStatus.GameStatus)
+		if status.GameStatus == "ended" {
+			fmt.Print("Game ended")
+			return
+		}
+		if status.ShouldFire {
+			handleOppShots(gameStatus, b)
+			b.Display()
+			fmt.Println("Your turn!")
+			handleYourShots(gameClient, b)
+		} else {
+			time.Sleep(1 * time.Second)
+		}
+	}
+}
 
+func handleYourShots(gameClient *client.GameClient, b *board.Board) {
+	var err error
+	var prompt string
+	yourShot, ok := board.ReadLineWithTimer(prompt, 60*time.Second)
+	if !ok {
+		fmt.Printf("There was a problem with reading line %v", ok)
+	}
+
+	fireResponse, err := gameClient.Fire(yourShot)
+	if err != nil {
+		fmt.Println(err)
+	}
+	SetRightBoard(fireResponse.Result, yourShot, b)
+}
+
+func handleOppShots(gameStatus *client.GameStatus, b *board.Board) {
+	for _, shot := range gameStatus.OppShots {
+
+		state, err := b.HitOrMiss(board.Left, shot)
 		if err != nil {
-			fmt.Printf("error getting game status : %s\n", err)
+			fmt.Println(err)
 		}
 
-		if gameStatus.GameStatus == "game_in_progress" {
+		err = b.Set(board.Left, shot, state)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+}
+
+func getGameStatus(gameClient *client.GameClient) *client.GameStatus {
+	var err error
+	gameStatus, err := gameClient.Status()
+
+	if err != nil {
+		fmt.Printf("error getting game status : %s\n", err)
+	}
+	return gameStatus
+}
+
+func getBoardGame(gameClient *client.GameClient) []string {
+	ships, err := gameClient.Board()
+	if err != nil {
+		fmt.Printf("error getting game board: %s\n", err)
+	}
+	return ships
+}
+
+func waitForGame(gameClient *client.GameClient) {
+	for {
+		status := getGameStatus(gameClient)
+
+		if status.GameStatus == "game_in_progress" {
 			break
 		}
 
 		time.Sleep(1 * time.Second)
-	}
-
-	boardStruct.Board, err = gameClient.Board()
-	if err != nil {
-		fmt.Printf("error getting game board: %s\n", err)
-		return
-	}
-
-	fmt.Println("Game status:", boardStruct)
-
-	b := board.New(board.NewConfig())
-	err = b.Import(boardStruct.Board)
-	if err != nil {
-		fmt.Println(err)
-	}
-	b.Display()
-
-	for {
-		gameStatus, err = gameClient.Status()
-
-		if err != nil {
-			fmt.Printf("error getting game status : %s\n", err)
-		}
-
-		if gameStatus.GameStatus == "ended" {
-			fmt.Print(gameStatus.LastGameStatus)
-			return
-		} else {
-			if gameStatus.ShouldFire {
-				for _, shot := range gameStatus.OppShots {
-
-					state, err := b.HitOrMiss(board.Left, shot)
-					if err != nil {
-						fmt.Println(err)
-					}
-
-					err = b.Set(board.Left, shot, state)
-					if err != nil {
-						fmt.Println(err)
-					}
-				}
-				b.Display()
-				fmt.Println("Your turn!")
-
-				var prompt string
-				yourShot, ok := board.ReadLineWithTimer(prompt, 60*time.Second)
-				if ok {
-				}
-
-				fireResponse, err = gameClient.Fire(yourShot)
-				if err != nil {
-					fmt.Println(err)
-				}
-
-				SetRightBoard(fireResponse.Result, yourShot, b)
-
-			} else {
-				time.Sleep(1 * time.Second)
-			}
-		}
 	}
 }
 
@@ -118,16 +123,3 @@ func SetRightBoard(s string, yourShot string, b *board.Board) {
 		}
 	}
 }
-
-// func GetState(s string){
-// 	switch s {
-// 	case "hit":
-// 		state := board.Hit
-// 	case "miss":
-// 		state := board.Miss
-// 	case "sunk":
-// 		state := board.Hit
-// 	}
-
-// 	return state
-// }
